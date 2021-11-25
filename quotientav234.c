361e5e01b1d5ea441bc7fe0fcf90af90964bda56
akhil varma <93900630+akhil-varma123@users.noreply.github.com>
2021-11-25 15:34:25 +0530

    Add files via upload

---
+++ quotientav234.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main()
+ 
+{
+    
+      int a,b,c,d;
+ 
+     printf("enter three values:");
+
+     scanf("%d",&d);
+
+     d=a+c*b;
+
+     printf("the quotient is:%d",d);
+
+    
+    return 0;
+
+}

275fd9c062198b48340b1881238456a62944b1ab
akhil varma <93900630+akhil-varma123@users.noreply.github.com>
2021-12-06 12:56:25 +0530

    Add files via upload

---
+++ a,b,c,d in row.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main()
+ {
+   
+       int i,j;
+ 
+      for(i=1;i<=26;i++)
+
+    {
+     
+           for(j=1;j<=i;j++)
+     
+            {
+         
+                printf("%c",'A'-1+i);
+      
+   }
+      
+      printf("\n");
+  
+  }
+  
+  return 0;
+
+}

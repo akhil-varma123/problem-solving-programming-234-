3752810ff58caa4e20f8044502717c67ed18d231
akhil varma <93900630+akhil-varma123@users.noreply.github.com>
2021-12-06 12:09:05 +0530

    Add files via upload

---
+++ do while table.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main()
+ {
+    
+          int i=1,n;
+     
+    printf("enter n to find the table:\n");
+  
+         scanf("%d",&n);
+   
+       do
+ 
+      {
+       
+             printf("%d*%d=%d\n",n,i,n*i);
+     
+        ++i;
+   
+        }while(i<=12);
+    
+  
+   
+    return 0;
+
+}

94a520c2c1420ca5d1b67a6660f918fd840cd84d
akhil varma <93900630+akhil-varma123@users.noreply.github.com>
2021-12-06 11:59:48 +0530

    Add files via upload

---
+++ do while.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main()
+ {
+  
+    int i=0,n;
+  
+    printf("enter the valu of n:\n");
+   
+    scanf("%d",&n);
+  
+  do
+   
+ {
+     
+    printf("%d\n",i);
+   
+      i++;
+ 
+   }while(i<n);
+   
+ 
+    return 0;
+
+}

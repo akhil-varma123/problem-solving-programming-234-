dbad87e48530ad0a5a2d4b5b87b478484bf5937d
akhil varma <93900630+akhil-varma123@users.noreply.github.com>
2021-12-13 12:29:19 +0530

    Add files via upload

---
+++ numbers in coloumn.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main()
+ {
+   
+       char i,j;
+ 
+      for(i=0;i<=5;i++)
+
+    {
+     
+           for(j=0;j<=i;j++)
+     
+            {
+         
+                printf("%c",i+49);
+      
+   }
+      
+      printf("\n");
+  
+  }
+  
+  return 0;
+
+}

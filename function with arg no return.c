04955862cbb36071c0c317793ad1fe1d2fb7072e
akhil varma <93900630+akhil-varma123@users.noreply.github.com>
2021-12-30 15:53:11 +0530

    Add files via upload

---
+++ function with arg no return.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int sum(int i,int j,int k)
+{
+    printf("sum=%d\n",i+j+k);
+}
+int sub(int i,int j,int k)
+{
+    printf("sub=%d\n",i-j-k);
+}
+int prod(int i,int j,int k)
+{
+    printf("prod=%d\n",i*j*k);
+}
+int main(){
+    int a,b,c;
+    printf("Enter three numbers:\n");
+    scanf("%d%d%d",&a,&b,&c);
+    sum(a,b,c);
+    sub(a,b,c);
+    prod(a,b,c);
+}
